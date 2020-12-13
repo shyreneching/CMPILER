1a5dfed790712a5177a1d095328e466ab0f723b7
ShyreneChing <shyrene_ching@dlsu.edu.ph>
2020-12-13 20:28:06 +0800

    fixed errors

--- MP Parser/src/Main.java
+++ MP Parser/src/Main.java
@@ -8,30 +8,31 @@ import gen.JavaLexer;
 import gen.JavaParser;
 
 import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
-//    public static void main(String[] args) {
-//        try {
-//            CharStream input = (CharStream) new ANTLRFileStream("test.simpler");
-//            JavaLexer lexer = new JavaLexer(input);
-//            JavaParser parser = new JavaParser(new CommonTokenStream(lexer));
-//            parser.addParseListener(new JavaCustomListener());
-//            parser.program();
-//        } catch (IOException ex) {
-//            Logger.getLogger(Java.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//
-//
-//    }
-    public static void main(String[] args) throws IOException {
-        CharStream input = (CharStream) new ANTLRFileStream("/input/test.txt");
-        JavaLexer lexer = new JavaLexer(input);
-        JavaParser parser = new JavaParser(new CommonTokenStream(lexer));
-        parser.addParseListener(new JavaCustomListener());
-        parser.enterClassDeclaration();
-        System.out.println(parser);
-        String name = parser.compilationUnit().getText();
-        System.out.println(name);
+    public static void main(String[] args) {
+        try {
+            CharStream input = (CharStream) new ANTLRFileStream("input/test.txt");
+            JavaLexer lexer = new JavaLexer(input);
+            JavaParser parser = new JavaParser(new CommonTokenStream(lexer));
+            parser.addParseListener(new JavaCustomListener());
+            parser.classDeclaration();
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
     }
+//    public static void main(String[] args) throws IOException {
+//        CharStream input = (CharStream) new ANTLRFileStream("/input/test.txt");
+//        JavaLexer lexer = new JavaLexer(input);
+//        JavaParser parser = new JavaParser(new CommonTokenStream(lexer));
+//        parser.addParseListener(new JavaCustomListener());
+//        parser.enterClassDeclaration();
+//        System.out.println(parser);
+//        String name = parser.compilationUnit().getText();
+//        System.out.println(name);
+//    }
 }
