c191b66fbc8f81c506f59aff5e13dbb1472c7fb7
ShyreneChing <shyrene_ching@dlsu.edu.ph>
2020-12-13 18:01:15 +0800

    Sample

--- MP Parser/src/Main.java
+++ MP Parser/src/Main.java
@@ -1,6 +1,15 @@
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
 
+import gen.JavaLexer;
+import gen.JavaParser;
 
 public class Main {
+    public static void main(String[] args){
+        JavaLexer lexer = new JavaLexer(CharStreams.fromString("int"));
+        JavaParser parser = new JavaParser(new CommonTokenStream(lexer));
 
-
+        String name = parser.primitiveType().getText();
+        System.out.println(name);
+    }
 }
